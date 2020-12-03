b78656bae1ee5f8059acf66b4e4d07ef3baa225d
prn04 <72257003+prn04@users.noreply.github.com>
2020-12-03 16:49:44 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,26 @@
+class Slingshot{
+    constructor(bodyA, bodyB){
+        var options = {
+            bodyA: bodyA,
+            pointB: bodyB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.slingshot = Constraint.create(options);
+        World.add(world, this.slingshot);
+    }
+
+    display(){
+        if (this.slingshot.bodyA) {
+            var pointA = this.slingshot.bodyA.position;
+        var pointB = this.slingshot.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+            
+        }
+         }
+    fly(){
+        this.slingshot.bodyA = null
+    }
+    
+}
